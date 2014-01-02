8381c3c35894f3022013d71209e41007a94e0af5
Otavio Carvalho <otaviolcarvalho@gmail.com>
2014-01-02 18:55:36 -0200

    Ajustes nos testes, refatorado SessionStorage

--- storages/SessionStorage.php
+++ storages/SessionStorage.php
@@ -6,23 +6,31 @@
  *  @author Otavio Carvalho <otaviolcarvalho@gmail.com>
  * 
  */
+
 namespace OvCart\Storage;
+
 use OvCart\Handlers\FileSessionHandler;
 
 class SessionStorage extends SessionStorageAbstract {
-    
+
     CONST FILE_HANDLER = "FileSessionHandler";
     CONST DEFAULT_HANDLER = "default";
-    
+
     public function __construct($name = 'default', $handler = 'default') {
-        
+        $this->setSessionHandler($handler);
+        parent::__construct($name);
+    }
+
+    public function setSessionHandler($handler) {
         switch ($handler) {
             case self::FILE_HANDLER:
                 parent::setSessionHandler(new FileSessionHandler());
                 break;
-            default:
+            case $handler instanceof \SessionHandlerInterface:
+                parent::setSessionHandler($handler);
                 break;
-        }        
-        parent::__construct($name);
+            default:               
+                break;
+        }
     }
 }
--- tests/unit/storages/SessionStorageTest.php
+++ tests/unit/storages/SessionStorageTest.php
@@ -1,40 +1,45 @@
 <?php
+
 /**
  * Description of SessionStorageTest
  *
  * @author Otavio Carvalho <otaviolcarvalho@gmail.com>
  * 
  */
-
 use OvCart\Storage\SessionStorage;
 
 class SessionStorageTest extends \PHPUnit_Framework_TestCase {
+
     /**
      *
      * @var SessionStorage
      */
     private $sessionStorage;
-    
+
     public function setUp() {
         $this->sessionStorage = new SessionStorage('Test');
-    }    
-    
-    public function testAdd() {      
-        $this->sessionStorage->add('teste', '123');        
+    }
+
+    public function testAdd() {
+        $this->sessionStorage->add('teste', '123');
         $this->assertTrue($this->sessionStorage->get('teste') == 123);
     }
-    public function testGetAll() {      
+
+    public function testGetAll() {
+        $this->sessionStorage->removeAll();
         $this->sessionStorage->add('teste', '123');
-        $this->sessionStorage->add('teste2', '23'); 
-        $this->sessionStorage->add('teste3', '13'); 
-        $this->assertCount(4,$this->sessionStorage->getAll());
+        $this->sessionStorage->add('teste2', '23');
+        $this->sessionStorage->add('teste3', '13');
+        $this->assertCount(3, $this->sessionStorage->getAll());
     }
+
     public function testRemove() {
         $this->sessionStorage->remove('teste');
         $this->assertFalse($this->sessionStorage->get('teste'));
     }
+
     public function testRemoveAll() {
         $this->sessionStorage->removeAll();
-        $this->assertCount(0,$this->sessionStorage->getAll());
+        $this->assertCount(0, $this->sessionStorage->getAll());
     }
 }
