117a06153cb9bb352b8ac51e415605d70a97cecf
Otavio Carvalho <otaviolcarvalho@gmail.com>
2014-01-03 14:17:07 -0200

    Adicionado comentarios nos metodos

--- session_handlers/FileSessionHandler.php
+++ session_handlers/FileSessionHandler.php
@@ -12,7 +12,14 @@ namespace OvCart\Handlers;
 class FileSessionHandler implements \SessionHandlerInterface {
 
     private $savePath;
-
+    
+    /**
+     * Tries to create a directory defined in php config
+     * 
+     * @param string $savePath
+     * @param string $name
+     * @return boolean
+     */
     public function open($savePath, $name) {
 
         $this->savePath = $savePath;
@@ -21,15 +28,28 @@ class FileSessionHandler implements \SessionHandlerInterface {
         }
         return true;
     }
-
+    /**
+     * 
+     * @return boolean
+     */
     public function close() {
         return true;
     }
-
+    /**
+     * Return content of the session id
+     * 
+     * @param mixed $id
+     * @return string
+     */
     public function read($id) {
         return (string) file_get_contents("$this->savePath/sess_$id");
     }
-
+    /**
+     * Remove the file of sesssion id
+     * 
+     * @param mixed $id
+     * @return boolean
+     */
     public function destroy($id) {
         $file = "$this->savePath/sess_$id";
         if (file_exists($file)) {
@@ -38,7 +58,13 @@ class FileSessionHandler implements \SessionHandlerInterface {
 
         return true;
     }
-
+    /**
+     * 
+     * Removes all files expired
+     * 
+     * @param int $maxlifetime
+     * @return boolean
+     */
     public function gc($maxlifetime) {
         foreach (glob("$this->savePath/sess_*") as $file) {
             if (filemtime($file) + $maxlifetime < time() && file_exists($file)) {
@@ -47,7 +73,13 @@ class FileSessionHandler implements \SessionHandlerInterface {
         }
         return true;
     }
-
+    /**
+     * Write data in file, create with session id (path/sess_1234)
+     * 
+     * @param mixed $id
+     * @param mixed$data
+     * @return int|boolean
+     */
     public function write($id, $data) {
         return file_put_contents("$this->savePath/sess_$id", $data) === false ? false : true;
     }
